1ec5be00fdb90d7c8efe67c2bfc4e6aa0ebce13d
Oksana Barinova <oksana.barinova@nangasystems.com>
2024-06-08 10:24:54 +0300

    add HandleUnauthorizedAccessException()

--- MaxSum/ReadFile.cs
+++ MaxSum/ReadFile.cs
@@ -22,11 +22,39 @@ namespace MaxSum
                     return allLines;
                 }
             }
+            
+            catch (UnauthorizedAccessException ex)
+            {
+                Log.Error($"Error: Access to the file is denied. {ex.Message}");
+                HandleUnauthorizedAccessException();
+                return allLines;
+            }
+
             catch (Exception ex)
             {
                 Log.Error($"An error occured while reading the file: {ex.Message}");
-                throw;
+                return allLines;
+            }
+        }
+
+        private void HandleUnauthorizedAccessException()
+        {
+            Console.WriteLine("Do you want to continue running the program? (yes/no)");
+            string userInput = Console.ReadLine().Trim().ToLower();
+
+            if (userInput == "no")
+            {
+                Console.WriteLine("Exiting program.");
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Please, enter file path manually:");
+                string filePath = @"" + Console.ReadLine();
+                Log.Debug("Console file path: {filePath}", filePath);
+                GetAllLines(filePath);
             }
         }
     }
 }
+
